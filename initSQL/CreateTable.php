c4286bb627984e5894377486098494d95bec6080
wtang13 <wtang13@hawk.iit.edu>
2017-09-27 17:05:50 -0500

    addClass:mysqlDBOperations

--- initSQL/CreateTable.php
+++ initSQL/CreateTable.php
@@ -1,8 +1,6 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * Function:read create sql from xml by tag<table>, the execute each xml
  */
-
+$xmlFile = '../files/generateReportConfiger.xml';
+$xml= simplexml_load_file($xmlFile);
